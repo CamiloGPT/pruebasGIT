cfdd5500b0a1431500f5b76ac3449db71e8c2f26
CamiloGPT <potes1997@hotmail.es>
2020-09-11 17:37:07 -0500

    [Add] new function formila in .js

--- js/main.js
+++ js/main.js
@@ -15,3 +15,11 @@ function dividir(a,b){
 function restar(a,b){
     return (a-b);
 }
+
+function formula(a,b) {
+    var x = sumar(a,b),
+        y = restar(a,b);
+    return(x/y);
+}
+
+formula(3,2);
